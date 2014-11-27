a3da4e4ff895f05df044259aa7828c8558984464
Sam Richards <samrichards@Sams-MacBook-Pro.local>
2014-11-27 22:03:42 +0000

    added a ;

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,35 @@
-angular.module('user', [])
-	.value('user', {
-		firstName: 'Samuel', 
-		middleName: 'Mark',
-		lastName: 'Richards',
-		loggedIn: false
-	});
+angular.module('xmpl.service', [])
 
-angular.module('homepageLogInCheck', ['user'])
-	.exists(function(user) {
-		return user === undefined;
-	})
-	.getUsersName(function(user) {
-		return user.firstName + " " + user.middleName + " " + user.lastName;
-	})
-	.isLoggedIn(function(user){
-		return user.loggedIn;
-	})
+  .value('greeter', {
+    salutation: 'Hello',
+    localize: function(localization) {
+      this.salutation = localization.salutation;
+    },
+    greet: function(name) {
+      return this.salutation + ' ' + name + '!';
+    }
+  })
+
+  .value('user', {
+    load: function(name) {
+      this.name = name;
+    }
+  });
+
+angular.module('xmpl.directive', []);
+
+angular.module('xmpl.filter', []);
+
+angular.module('homepageLogInCheck', ['xmpl.service', 'xmpl.directive', 'xmpl.filter'])
+
+  .run(function(greeter, user) {
+    // This is effectively part of the main method initialization code
+    greeter.localize({
+      salutation: 'Bonjour'
+    });
+    user.load('World');
+  })
+
+  .controller('XmplController', function($scope, greeter, user){
+    $scope.greeting = greeter.greet(user.name);
+  });
