df4df241d2dcc004375d3eb4d8a48115ebe28b5d
Sam Richards <samrichards@Sams-MacBook-Pro.local>
2014-11-29 21:07:18 +0000

    fixed some form validation onto the register and the log in page - also fixed some css regarding the pages

--- app/js/controllers/logInPage.js
+++ app/js/controllers/logInPage.js
@@ -1,9 +1,23 @@
 angular.module('homepageLogInCheck').controller('logInPage', function($scope, user) {
 	
 	$scope.username = user.username,
+	$scope.password = user.password,
+	$scope.registerUsername = "", $scope.registerEmail = "", $scope.registerPassword = "", $scope.registerPoop = "",
 
 	$scope.showLoginForm = true,
-	$scope.toggleLogInClass = function($scope) {
+	$scope.toggleLogInClass = function() {
 		this.showLoginForm = !this.showLoginForm;
+	},
+	$scope.submitLogIn = function() {
+		console.log('submit log in');
+		console.log($scope.username);
+		console.log($scope.password);
+	}, 
+	$scope.submitRegister = function() {
+		console.log('register');
+		console.log($scope);
+		console.log($scope.registerUsername);
+		console.log($scope.registerEmail);
+		console.log($scope.registerPassword);
 	}
 });
